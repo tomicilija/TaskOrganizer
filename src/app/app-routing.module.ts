94f1d1208b233e8162a678af994516564f9f3321
tomicilija <ilijatomic17@gmail.com>
2022-07-12 17:22:07 +0200

    Main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
   }
 ];
 @NgModule({
