1e6ef0f7262592f6c538f109d3f4fc7ff80f4437
tomicilija <ilijatomic17@gmail.com>
2022-07-13 00:18:15 +0200

    DatePicker works

--- src/app/monthly-view/monthly-view.page.ts
+++ src/app/monthly-view/monthly-view.page.ts
@@ -6,9 +6,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monthly-view.page.scss'],
 })
 export class MonthlyViewPage implements OnInit {
+  private tasks: {
+    title: string;
+    date: string;
+    time: string;
+    duration: string;
+    place: string;
+    note: string;
+    alert: boolean;
+  }[] = [
+    {
+      title: 'Meeting with Anomali Team',
+      date: '2022-07-01',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: 'Be ready',
+      alert: false,
+    },
+    {
+      title: 'Dinner with Anna',
+      date: '2022-07-01',
+      time: '12:00',
+      duration: '1',
+      place: "Anna's House",
+      note: "don't forget to give her a bouquet of flowers",
+      alert: false,
+    },
+    {
+      title: 'Pay House Tax',
+      date: '2022-07-05',
+      time: '12:00',
+      duration: '2',
+      place: '',
+      note: '',
+      alert: false,
+    },
+    {
+      title: 'Make A New Propsal',
+      date: '2022-07-05',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: '',
+      alert: false,
+    },
+    {
+      title: 'Meeting With A New Client',
+      date: '2022-07-12',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: '',
+      alert: false,
+    },
+    {
+      title: 'Meeting With An Old Client',
+      date: '2022-07-22',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: '',
+      alert: false,
+    },
+  ];
+
+  private dateValue: any;
 
   constructor() { }
 
+  get datePicked(): any {
+    return this.dateValue;
+  }
+  set datePicked(value: any) {
+    console.log({ value });
+    this.dateValue = value;
+  }
   ngOnInit() {
   }
 
--- src/app/task-list/task-list.page.ts
+++ src/app/task-list/task-list.page.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.page.scss'],
 })
 export class TaskListPage implements OnInit {
+  private tasks: {
+    title: string;
+    date: string;
+    time: string;
+    duration: string;
+    place: string;
+    note: string;
+    alert: boolean;
+  }[] = [
+    {
+      title: 'Meeting with Anomali Team',
+      date: '2020-01-01',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: 'Be ready',
+      alert: false,
+    },
+    {
+      title: 'Dinner with Anna',
+      date: '2020-01-01',
+      time: '12:00',
+      duration: '1',
+      place: "Anna's House",
+      note: "don't forget to give her a bouquet of flowers",
+      alert: false,
+    },
+    {
+      title: 'Pay House Tax',
+      date: '2020-01-01',
+      time: '12:00',
+      duration: '2',
+      place: '',
+      note: '',
+      alert: false,
+    },
+    {
+      title: 'Make A New Propsal',
+      date: '2020-01-01',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: '',
+      alert: false,
+    },
+    {
+      title: 'Meeting With A New Client',
+      date: '2020-01-01',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: '',
+      alert: false,
+    },
+    {
+      title: 'Meeting With An Old Client',
+      date: '2020-01-01',
+      time: '07:00',
+      duration: '3',
+      place: 'Anomali Office',
+      note: '',
+      alert: false,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
